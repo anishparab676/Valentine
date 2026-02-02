0f036e2a5f712845e47483264385ddf1ae7a87c8
anishparab676 <anishparab676@gmail.com>
2026-02-02 19:36:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+let messageIndex = 0;
+const messages = [
+  "Are you sure?",
+  "Really sure?",
+  "Think again!",
+  "Last chance!",
+  "Surely not?",
+  "You might regret this!",
+  "Give it another thought!",
+  "Are you absolutely sure?",
+  "This could be a mistake!",
+  "Have a heart!",
+  "Don't be so cold!",
+  "Change of heart?",
+  "Wouldn't you reconsider?",
+  "Is that your final answer?",
+  "You're breaking my heart ;("
+];
+
+function handleNoClick() {
+  const noButton = document.querySelector('.no-button');
+  const yesButton = document.querySelector('.yes-button');
+
+  // Update the text of the No button
+  noButton.textContent = messages[messageIndex];
+
+  // Increment index and loop back to 0 if at the end
+  messageIndex = (messageIndex + 1) % messages.length;
+
+  // Make the Yes button grow
+  const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+  yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
